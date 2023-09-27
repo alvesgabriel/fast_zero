26b80c5968502b40bf2972dae3153e374991e7b9
Gabriel Alves <gabriel.alves.monteiro1@gmail.com>
2023-09-26 21:02:48 -0300

    Updated endpoints to use real database

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,3 +1,6 @@
+from fast_zero.schemas import UserPublic
+
+
 def test_root_return_200_and_hello_world(client):
     response = client.get('/')
 
@@ -23,24 +26,35 @@ def test_create_user(client):
     }
 
 
+def test_create_user_already_exist(client, user):
+    response = client.post(
+        '/users/',
+        json={
+            'username': 'test',
+            'email': 'alice@example.com',
+            'password': 'secret',
+        },
+    )
+
+    assert response.status_code == 400
+
+
 def test_read_users(client):
     response = client.get('/users/')
 
     assert response.status_code == 200
-    assert response.json() == {
-        'users': [
-            {
-                'username': 'alice',
-                'email': 'alice@example.com',
-                'id': 1,
-            }
-        ]
-    }
+    assert response.json() == {'users': []}
 
 
-def test_update_user(client):
+def test_read_user_with_users(client, user):
+    user_schema = UserPublic.model_validate(user).model_dump()
+    response = client.get('/users/')
+    assert response.json() == {'users': [user_schema]}
+
+
+def test_update_user(client, user):
     response = client.put(
-        '/users/1',
+        f'/users/{user.id}',
         json={
             'username': 'alice.wonderland',
             'email': 'alice@wonderland.com',
@@ -70,8 +84,8 @@ def test_update_invalid_user(client):
     assert response.json() == {'detail': 'User not found'}
 
 
-def test_delete_user(client):
-    user_id = 1
+def test_delete_user(client, user):
+    user_id = user.id
     response = client.delete(f'/users/{user_id}')
 
     assert response.status_code == 200
